2fb3626f9515d3e5d0c27a58b12477e709aefb39
Toro <l.email.d.oasis@gmail.com>
2026-03-27 17:08:58 +0100

    v2.5.2

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs      = require('fs');
-const path    = require('path');v
+const path    = require('path');
 const client  = require('./client');
 
 const app = express();
