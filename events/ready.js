2d24c2fc6bb1ec780ffedbfb174a8217381d9d59
Toro <l.email.d.oasis@gmail.com>
2026-03-27 15:48:05 +0100

    v2.4.0 Ajout statuts du bot

--- events/ready.js
+++ events/ready.js
@@ -11,6 +11,36 @@ module.exports = {
         await loadConfig();
         loadTickets();
         startScheduler(client);
+
         console.log(`✅ ${client.user.tag} prêt.`);
+
+        // 🎯 Liste de statuts
+        const statuses = [
+            { type: 0, getName: (guild) => `Bot du serveur 🪽﹒Eldoria !` },
+            { type: 3, getName: (guild) => `${guild.memberCount} aventuriers.. Deviens l'un d'entre eux !` },
+            { type: 3, getName: (guild) => `Lien du serveur en Bio !` }
+        ];
+
+        let i = 0;
+
+        const updateStatus = () => {
+            const guild = client.guilds.cache.first();
+            if (!guild) return;
+
+            const status = statuses[i];
+
+            client.user.setPresence({
+                activities: [{
+                    name: status.getName(guild),
+                    type: status.type
+                }],
+                status: 'online'
+            });
+
+            i = (i + 1) % statuses.length;
+        };
+
+        updateStatus();
+        setInterval(updateStatus, 5 * 60 * 1000);
     },
 };
